3c15bceedfea3d09949ed2db3d86c306dcfd5719
emelcd <mick.altura@gmail.com>
2021-09-07 13:29:47 +0200

    Upgraded UI/UX

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import './Products.css'
+import "./Products.css";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -14,36 +14,33 @@ const Products = () => {
 
   return (
     <div className="card-container">
-        {products.map((product) => (
-          <div key={product.id} className="card">
-            <img
-              src={product.image}
-              alt=""
-            />
-            <div className="card-body">
-              <div className="row">
-                <div className="card-title">
-                  <h4>{product.title}</h4>
-                  <h3>${product.price}</h3>
-                </div>
-                <div className="view-btn">
-                  <a href="">View Details</a>
-                </div>
+      {products.map((product) => (
+        <div key={product.id} className="card">
+          <img src={product.image} alt="" />
+          <div className="card-body">
+            <div className="row">
+              <div className="card-title">
+                <h4>{product.title}</h4>
+                <h3>${product.price}</h3>
               </div>
-              <hr />
-              <p className="card-description">
-                {product.description}
-              </p>
-              <div className="btn-group">
-                <div className="btn">
-                  <a href="">Buy Now</a>
-                </div>
-                <a href=""> Cancel</a>
+              <div className="view-btn">
+                <a href="">Details</a>
               </div>
             </div>
+            <div className="card-footer">
+
+            <div className="card-description">{product.description}</div>
+            <div className="btn-group">
+              <div className="btn">
+                <a href="">Buy Now</a>
+              </div>
+              <a href=""> Cancel</a>
+            </div>
+            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
